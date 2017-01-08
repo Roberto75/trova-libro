5332d7ff829ea1b30abd8e8ddfe8813338d771b9
Daitarn <roberto.rutigliano@gmail.com>
2017-01-08 20:02:04 +0100

    ok

--- sito4.0/App_Start/RouteConfig.cs
+++ sito4.0/App_Start/RouteConfig.cs
@@ -13,10 +13,13 @@ namespace MyWebApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+           
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }, // Parameter defaults
+                  new[] { "MyWebApplication.Controllers" } //Namespace
             );
         }
     }
