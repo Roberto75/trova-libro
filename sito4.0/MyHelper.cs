645ed71f44f478e7de28728699a944ac5afe8d97
Daitarn <roberto.rutigliano@gmail.com>
2017-04-09 12:22:02 +0200

    registrazione

--- sito4.0/MyHelper.cs
+++ sito4.0/MyHelper.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 
 public class MyHelper
-    {
+{
 
     public static void printRequest(System.Web.HttpRequest request)
     {
@@ -26,4 +26,101 @@ public class MyHelper
         }
 
     }
+
+
+
+
+    #region "Decode NULL"
+
+
+
+    public static HtmlString decodeNull(DateTime? valore)
+    {
+
+        if (valore == null || valore == DateTime.MinValue)
+        {
+            return new HtmlString("N/A");
+        }
+
+
+        return new HtmlString(valore.ToString());
+    }
+
+    public static HtmlString decodeNull(bool? valore)
+    {
+
+        if (valore == null)
+        {
+            return new HtmlString("N/A");
+        }
+
+
+        return new HtmlString(valore.ToString());
+    }
+
+
+    public static HtmlString decodeNull(string valore)
+    {
+
+        if (String.IsNullOrEmpty(valore))
+        {
+            return new HtmlString("N/A");
+        }
+
+
+        return new HtmlString(valore.Replace(Environment.NewLine, "<br />"));
+    }
+
+    public static HtmlString decodeNull(decimal? valore)
+    {
+
+        if (valore == null)
+        {
+            return new HtmlString("N/A");
+        }
+
+
+        return new HtmlString(valore.Value.ToString());
+    }
+
+
+    public static HtmlString decodeNull(int? valore)
+    {
+
+        if (valore == null)
+        {
+            return new HtmlString("N/A");
+        }
+
+
+        return new HtmlString(valore.Value.ToString());
+    }
+
+
+    public static HtmlString decodeEnum(string value)
+    {
+        if (String.IsNullOrEmpty(value) || (value == "0") || (value == "Undefined"))
+        {
+            return new HtmlString("N/A");
+        }
+
+
+        return new HtmlString(value.Replace("_", " "));
+    }
+
+
+    public static HtmlString decodeNull(double? valore)
+    {
+
+        if (valore == null)
+        {
+            return new HtmlString("N/A");
+        }
+
+
+        return new HtmlString(valore.Value.ToString());
+    }
+
+    #endregion
+
 }
