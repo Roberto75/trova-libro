477d07311201229d63d6a3b2b10b5831f3bb8a07
Daitarn <Roberto75@users.noreply.github.com>
2022-02-27 17:24:44 +0100

    upgrade bootstrap + summernote + lightbox

---
+++ sito4.0/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Collapse()
+        {
+            return View();
+        }
+
+        public ActionResult Modal()
+        {
+            return View();
+        }
+
+        public ActionResult Summernote()
+        {
+            return View();
+        }
+    }
+}
---
+++ sito4.0/README.md
@@ -0,0 +1,4 @@
+﻿UPGRADE Boostrap 5.1
+data-toggle to data-bs-toggle
+data-dismiss to data-bs-dismiss
+data-target to data-bs-target
